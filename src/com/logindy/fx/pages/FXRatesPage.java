c2f18c5c448c39371120f8e7dbd4e7b8ea1d9f35
krit kasemosoth <krit_kasemosoth@hotmail.com>
2015-12-13 15:19:21 +0700

    Updated the FX Rates URL. Added the steps to find and verify the HTML elements for the FX Rates display.

--- src/com/logindy/fx/pages/FXRatesPage.java
+++ src/com/logindy/fx/pages/FXRatesPage.java
@@ -1,16 +1,19 @@
 package com.logindy.fx.pages;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 
 public class FXRatesPage {
-	public static final String FXRatesUrl = "http://www.logindy.com/fx/rates";
+	public static final String FXRatesUrl = "http://www.logindy.com/fx-rates.html";
 
 	@FindBy(how = How.CLASS_NAME, className = "rate-panel")
 	public List<WebElement> ratePanels;
@@ -18,9 +21,16 @@ public class FXRatesPage {
 	public void verifyFXRates(int orderNo, String pair, String buy, String sell) {
 		assertThat(ratePanels, notNullValue());
 		
-		WebElement rates = ratePanels.get(orderNo);
+		WebElement ratePanel = ratePanels.get(orderNo);
+		assertThat(ratePanel, notNullValue());
 		
-		assertThat(rates, notNullValue());
-		assertThat(rates.getText(), equalTo(pair));
+		WebElement currencyPair = ratePanel.findElement(By.className("rate-panel__currency-pair"));
+		assertThat(currencyPair.getText(), equalTo(pair));
+		
+		WebElement buyRate = ratePanel.findElement(By.className("rate-panel__buy-rate"));
+		assertThat(buyRate.getText(), equalTo(buy));
+		
+		WebElement sellRate = ratePanel.findElement(By.className("rate-panel__sell-rate"));
+		assertThat(sellRate.getText(), equalTo(sell));		
 	}
 }
